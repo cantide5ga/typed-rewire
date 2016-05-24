8a3c5ec07bf22f5aaa0ee60168ef174a8d17e241
Michael Neil Payne <thruston82@rocketmail.com>
2016-05-24 14:15:45 -0400

    wrapped in namespace

--- rewire.d.ts
+++ rewire.d.ts
@@ -1,10 +1,12 @@
-declare function rewire(file: string): {
-    __get__(fn: string): () => any;
-    __set__(variable: string, mock: any): void;
-    __set__(variables: { [variable: string]: any }): void
-    __with__(variables: { [variable: string]: any }): () => any
+declare namespace Rewire {
+    function rewire(file: string): {
+        __get__(fn: string): () => any;
+        __set__(variable: string, mock: any): void;
+        __set__(variables: { [variable: string]: any }): void
+        __with__(variables: { [variable: string]: any }): () => any
+    }
 }
 
-export = rewire;
+export = Rewire;
 
 
