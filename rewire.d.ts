69d8069ce84a10928206786ad05e2772ff9576ca
Michael Neil Payne <thruston82@rocketmail.com>
2016-05-25 14:10:19 -0400

    adding interface for external use

--- rewire.d.ts
+++ rewire.d.ts
@@ -1,14 +1,15 @@
-//declare namespace Rewire {
-    declare function rewire<T>(file: string): T & {
+declare namespace rewire {
+    interface Rewire {
         __get__(fn: string): () => any;
         __set__(variable: string, mock: any): void;
         __set__(variables: { [variable: string]: any }): void
         __with__(variables: { [variable: string]: any }): () => any
-    }
-    
-    export = rewire;
-//}
+    }    
+}
 
-//export = Rewire;
+declare function rewire<T>(file: string): T & rewire.Rewire;
+   
+export = rewire;
+
 
 
