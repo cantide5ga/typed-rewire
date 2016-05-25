ab4ea9c402e768bcd65ef6a5703be59474450a0b
Michael Neil Payne <thruston82@rocketmail.com>
2016-05-25 14:02:13 -0400

    export function only

--- rewire.d.ts
+++ rewire.d.ts
@@ -1,10 +1,12 @@
 //declare namespace Rewire {
-    export = function rewire<T>(file: string): T & {
+    declare function rewire<T>(file: string): T & {
         __get__(fn: string): () => any;
         __set__(variable: string, mock: any): void;
         __set__(variables: { [variable: string]: any }): void
         __with__(variables: { [variable: string]: any }): () => any
     }
+    
+    export = rewire;
 //}
 
 //export = Rewire;
